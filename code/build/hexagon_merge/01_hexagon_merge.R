67d0f2757dd5a38e0a4e0e797721744eee312a7f
hbauermitterlehner <hbm@uchicago.edu>
2025-02-18 21:21:29 -0600

    update code

--- code/build/hexagon_merge/01_hexagon_merge.R
+++ code/build/hexagon_merge/01_hexagon_merge.R
@@ -29,30 +29,95 @@ redlines <- st_transform(redlines, st_crs(redlines))
 
 #Join Data -------------------------
 
-#Housing Projects 
-hexhomshp_merged <- st_join(hex, cha_projects, join = st_contains, left = TRUE)  %>% 
-  mutate(across(name:id, ~ ifelse((year >= yr_built_start & year <= yr_built_end) | 
-           (year >= yr_dmlsh_or_cnvrt_start & year <= yr_dmlsh_or_cnvrt_end), ., NA)))
 
 
-#Riots
-hexhomshp_merged <- st_join(hexhomshp_merged, riots, join = st_contains, left = TRUE, suffix =c("", "_riot")) %>% 
-  mutate(across(id_riot:year_riot, ~ ifelse((year == year_riot), ., NA)))
+##Housing Projects --------------------
+hexhomshp_merged <- st_join(hex, cha_projects %>% 
+                              rename_all(~paste0(., "_prjct"))
+                            , join = st_contains, left = TRUE)  %>% 
+  mutate(across(name_prjct:id_prjct, ~ ifelse((year >= yr_built_start_prjct & year <= yr_built_end_prjct ) | 
+                                                (year == yr_built_start_prjct & is.na(yr_built_end_prjct)) |
+                                              (year >= yr_dmlsh_or_cnvrt_start_prjct   & year <= yr_dmlsh_or_cnvrt_end_prjct) | 
+                                                (year == yr_dmlsh_or_cnvrt_start_prjct  & is.na(yr_dmlsh_or_cnvrt_end_prjct)) | 
+                                                (is.na(yr_dmlsh_or_cnvrt_start_prjct) & year == yr_dmlsh_or_cnvrt_end_prjct), ., NA)))
 
-#Landsales 
 
-hexhomshp_merged_land <- st_join(hexhomshp_merged, landsales, join = st_intersects, left = TRUE)
+
+# hexhomshp_merged %>% filter(!is.na(name_prjct)) %>% View()
+
+## Riots ---------------------------
+hexhomshp_merged <- st_join(hexhomshp_merged, riots %>% 
+                              rename_all(~paste0(., "_riot")), join = st_contains, left = TRUE) %>% 
+  mutate(across(id_riot:name_riot, ~ ifelse((year == year_riot), ., NA)))
+
+# hexhomshp_merged %>% filter(!is.na(name_riot)) %>% View()
+
+
+## Landsales -----------------------------------
+landsales_mod <- landsales %>% 
+  dplyr::select(geometry, in_checkerboard)
+
+hexhomshp_merged <- hexhomshp_merged %>% 
+  mutate(id_merge = row_number())
+
+hexhomshp_merged_land <- st_join(hexhomshp_merged, landsales_mod, join = st_intersects, left = TRUE) 
+
+hexhomshp_merged_land <-hexhomshp_merged_land %>%
+  st_drop_geometry() %>% 
+  distinct(id_merge, in_checkerboard) %>% 
+  mutate(in_checkerboard = ifelse(is.na(in_checkerboard), FALSE ,in_checkerboard ))
+
+hexhomshp_merged <- left_join(hexhomshp_merged, hexhomshp_merged_land) 
+
+
+## Redlines -------------------------------------------------
+
+## ASK ABOUT THUS
+sf_use_s2(FALSE)
+
+intersect_redlines <- st_intersection(hexhomshp_merged, redlines) 
+
+intersect_redlines <- intersect_redlines %>%
+  mutate(intersect_area = st_area(.))
+
+intersect_redlines <- intersect_redlines %>% 
+  st_drop_geometry() %>% 
+  distinct(id_merge, category, intersect_area) %>% 
+  mutate(category = str_replace_all(category, " ", "_")) %>% 
+  # group_by(id_merge, category) %>% 
+  # summarise(intersect_area = sum(intersect_area)) %>% 
+  # ungroup() %>% 
+  pivot_wider(id_cols = id_merge, names_from = category, values_from = intersect_area) 
+
+intersect_redlines <- intersect_redlines %>% 
+  mutate(Best = map_dbl(Best, ~ sum(unlist(.x))), 
+         Still_Desirable = map_dbl(Still_Desirable, ~ sum(unlist(.x))), 
+         Definitely_Declining = map_dbl(Definitely_Declining, ~ sum(unlist(.x))),
+         Hazardous = map_dbl(Hazardous, ~ sum(unlist(.x))),
+         Industrial = map_dbl(Industrial, ~ sum(unlist(.x))),
+         Commercial = map_dbl(Commercial, ~ sum(unlist(.x))))
+
+
+sf_use_s2(TRUE )
+
+hexhomshp_merged <- left_join(hexhomshp_merged, intersect_redlines, by= c("id_merge")) 
+
+hexhomshp_merged <- hexhomshp_merged %>% 
+  mutate(polygon_area = st_area(.)) %>% 
+  mutate(across(Best:Commercial, ~round(./as.numeric(polygon_area), 2))) %>% 
+  mutate(sum_check=  Best + Still_Desirable + Definitely_Declining + Hazardous + Industrial + Commercial)
 
 
 #Plots ---------
 
-ggplot(data = landsales %>% filter(in_checkerboard == TRUE) %>% head(30)) +
-  geom_sf(aes(fill = in_checkerboard)) +
+ggplot(data = test %>% filter(year == 1874)) +
+  geom_sf(aes(fill = sum_check)) +
   theme_minimal() + 
   coord_sf(
     xlim = c(-87.94011, -87.52398), # Approximate longitude bounds of Chicago
     ylim = c(41.64454, 42.02304)    # Approximate latitude bounds of Chicago
   ) 
+
 
 colors <- setNames(unique(redlines$fill), unique(redlines$category))
 ggplot(data = redlines %>% filter(city == "Chicago")) +
