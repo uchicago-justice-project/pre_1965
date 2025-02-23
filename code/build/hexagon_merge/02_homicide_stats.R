fdf5a52b6457ef1de226cabe18bb9a8710624094
hbauermitterlehner <hbm@uchicago.edu>
2025-02-23 17:53:18 -0600

    updates to investigation

--- code/build/hexagon_merge/02_homicide_stats.R
+++ code/build/hexagon_merge/02_homicide_stats.R
@@ -8,22 +8,22 @@ df <- st_read("data/mst/hex_merged.gpkg")
 
 # HOMICIDE STATS ---------------------------
 
-hexhomshp_10 <- df %>% 
+hexhomshp_5 <-  df %>% 
   st_drop_geometry() %>% 
   dplyr::select(GRID_ID, hom_ct, hom_rt, year) %>% 
-  mutate(year_10 = ceiling((year+5)/10)*10-5) %>% 
-  group_by(GRID_ID, year_10) %>% 
-  mutate(across(c(hom_ct, hom_rt), .fns = list(sum_10 = ~sum(.), 
-                                                          mean_10 = ~mean(.), 
-                                                          min_10 = ~min(.), 
-                                                          max_10 = ~max(.), 
-                                                          median_10 = ~median(.)))) %>% 
+  mutate(year_5 = 5 * ceiling(year / 5)) %>% 
+  group_by(GRID_ID, year_5) %>% 
+  mutate(across(c(hom_ct, hom_rt), .fns = list(sum_5 = ~sum(.), 
+                                               mean_5 = ~mean(.), 
+                                               min_5 = ~min(.), 
+                                               max_5 = ~max(.), 
+                                               median_5 = ~median(.)))) %>% 
   ungroup() %>% 
   dplyr::select( -hom_ct,  -hom_rt) 
 
 #hexhomshp_10 %>% filter(GRID_ID == "AK-22" & year_10 == 1945) %>% View()
 
-df_out <- left_join(df, hexhomshp_10, by = c("GRID_ID", "year"))
+df_out <- left_join(df, hexhomshp_5, by = c("GRID_ID", "year"))
 
 sf_object <- st_as_sf(df_out, wkt = "geometry") 
 
