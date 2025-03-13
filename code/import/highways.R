ab081acfb752fe438ad592c341c7842ed5d40194
hbauermitterlehner <hbm@uchicago.edu>
2025-03-13 09:04:12 -0500

    update to analysis

--- code/import/highways.R
+++ code/import/highways.R
@@ -4,13 +4,13 @@ source("header.R")
 # Data Import ---------------------
 
 streets <- st_read("data/raw/streets")
+highway_cons <- read_csv("data/raw/highway_construction.csv")
 
+# Selection -------------------------
 select_hways <- streets %>% 
   filter(DEDICATED_ %in% c("ADLAI STEVENSON EXPY", 
                            "BISHOP FORD FREEWAY", 
-                           "CHICAGO-OHARE INT'L", 
                            "CHICAGO SKWY", 
-                           "CONGRESS ST", 
                            "DAN RYAN EXPY", 
                            "EAST-WEST TLWY", 
                            "EDENS EXPY", 
@@ -19,11 +19,12 @@ select_hways <- streets %>%
                            "NORTHWEST TLWY", 
                            "TRI-STATE TLWY")) 
 
+merge <- select_hways %>% 
+  left_join(highway_cons, by = c("DEDICATED_" = "name"))
 
-ggplot(data = select_hways) + 
-  geom_sf(aes(color = as.factor(DEDICATED_)))
+# Save -------------------------------------
 
-sf_object <- st_as_sf(select_hways, wkt = "geometry") 
+sf_object <- st_as_sf(merge, wkt = "geometry") 
 
 st_write(sf_object,"data/intermediate/highways.gpkg", layer = "highways", append = FALSE)
 
@@ -61,5 +62,4 @@ highways %>%
   distinct(TYPE,HWYTYPE,  St_PosTyp, DEDICATED_) %>% 
   View()
 
-colnames(streets
-        )
+colnames(streets)
